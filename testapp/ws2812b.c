5a1eaa123bdccc08e7c84de97a86a365f7aa7a1d
Elliot Williams <elliot@pre.postero.us>
2015-08-14 18:12:40 +0200

    speed test ws2812 with improved display

--- testapp/ws2812b.c
+++ testapp/ws2812b.c
@@ -10,6 +10,17 @@
 
 //# MENU ws2812b
 void ws1812b_menu(){
+    uint8_t pattern2[] = {
+                     0, 255, 0,
+                     0, 255, 0,
+
+                     128,   0,   255,
+                     128,   0,   255,
+                     128,   0,   255,
+                     128,   0,   255,
+                     128,   0,   255,
+                     255, 0,   128
+                     };
     uint8_t pattern[] = {
                      255, 255, 0,
                      255, 255, 0,
@@ -25,15 +36,18 @@ void ws1812b_menu(){
     uint8_t green[] = {255, 0, 0};
 	getInputWaitRelease();
     //cpu_clock_set(17);
-    cpu_clock_set(51);
 	SETUPgout(RGB_LED);
 
+	uint8_t speed=51;
+cpu_clock_set(speed);
 	while(1){
 		lcdClear(0xff);
 		lcdPrintln("WS2812B test");
 		lcdPrintln("UP: pattern");
 		lcdPrintln("DOWN: green");
 		lcdPrintln("ENTER: exit");
+		lcdPrintln("Speed:");
+		lcdPrintln(IntToStr(speed, 3, ' '));
 		lcdDisplay();
 
 		switch(getInput()){
@@ -41,11 +55,15 @@ void ws1812b_menu(){
                 ws2812_sendarray(pattern, sizeof(pattern));
 				break;
 			case BTN_DOWN:
-                ws2812_sendarray(green, sizeof(green));
+                ws2812_sendarray(pattern2, sizeof(pattern2));
 				break;
 			case BTN_LEFT:
+				--speed;
+		cpu_clock_set(speed);
 				break;
 			case BTN_RIGHT:
+				++speed;
+		cpu_clock_set(speed);
 				break;
 			case BTN_ENTER:
 				return;
